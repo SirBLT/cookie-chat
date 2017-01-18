903a7bff8b298888f9e6c4c105c4bdad73e3ff9f
SirBLT <blt8324@gmail.com>
2017-01-18 15:13:17 -0700

    Cookie chat project

--- js/messageService.js
+++ js/messageService.js
@@ -1,4 +1,20 @@
-angular.module('chatroom').service('messageService', function($http){
+angular.module('chatroom')
+.service('messageService', function($http){
+
+  this.getMessages = function() {
+    return $http({
+      method: 'GET',
+      url: 'https://practiceapi.devmounta.in/api/chats'
+    });
+  }
+
+  this.postMessages = function(msg) {
+    return $http({
+      method: 'POST',
+      url: 'https://practiceapi.devmounta.in/api/chats',
+      data: {message: msg}
+    })
+  }
   //Here you'll need to create two methods. One called postMessage and the other called getMessages.
 
   //On the lines below create a getMessages method. This method will retrieve data from the backend.
